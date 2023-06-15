19d387133102eb8c7469480c2ff15948eb0d131a
Tyler Herron <tylerherron7777@gmail.com>
2023-06-15 12:32:04 -0500

    startup

--- java/startup/src/main/java/com/start/run.java
+++ java/startup/src/main/java/com/start/run.java
@@ -1,23 +1,40 @@
 package com.start;
-
-import java.awt.Desktop;
+import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.io.File;
-import java.io.IOException;
 
 public class run {
-    public static void main(String[] args) {
-        create("C:\\Users\\Thewi\\Desktop\\Sage - Poe.lnk");
-        create("C:\\Users\\Thewi\\Desktop\\XAMPP Control Panel.lnk");
-        create("C:\\Users\\Thewi\\Desktop\\Visual Studio Code.lnk");
+    public static void main(String[] args) throws Exception {
+        Thread t1 = new Thread(() -> create("C:\\Users\\Thewi\\Desktop\\Sage - Poe.lnk", true));
+        Thread t2 = new Thread(() -> create("C:\\Users\\Thewi\\Desktop\\XAMPP Control Panel.lnk", true));
+        Thread t3 = new Thread(() -> create("C:\\Users\\Thewi\\Desktop\\Visual Studio Code.lnk", false)); // don't minimize Visual Studio Code
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
     }
 
-    public static void create(String path) {
+    public static void create(String path, boolean minimize) {
         String shortcutPath = path; // Replace with the path to your shortcut file
         File shortcutFile = new File(shortcutPath);
 
         try {
             Desktop.getDesktop().open(shortcutFile);
-        } catch (IOException e) {
+            Thread.sleep(500); // Wait for the application to fully open
+
+            if (minimize) {
+                Robot robot = new Robot();
+                robot.keyPress(KeyEvent.VK_WINDOWS); // Simulate the Windows key press
+                robot.keyPress(KeyEvent.VK_D); // Simulate the D key press
+                robot.delay(100); // Wait for a short time
+                robot.keyRelease(KeyEvent.VK_D); // Release the D key
+                robot.keyRelease(KeyEvent.VK_WINDOWS); // Release the Windows key
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
